07dd4c0d5720f550e8db351abd83ab04832a32fb
natnaeldejenekebede <110912463+natnaeldejenekebede@users.noreply.github.com>
2024-03-09 23:05:22 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { CommandDemo } from "./Command";
 export default function Header(){
-    return <div className="grid grid-cols-2 gap-4  border-b   p-4">
+    return <div className="grid grid-cols-2 gap-4  border-b  p-5">
     <CommandDemo/>
 </div>
 }
