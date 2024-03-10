22a7e9f4be8203db0f936e1a9d6fb4a2ecdcbf2b
natnaeldejenekebede <natnaeldejene19@gmail>
2024-03-10 15:49:38 +0300

    add new

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,15 +32,18 @@ export default function Header() {
                 <DropdownMenu>
                     <DropdownMenuTrigger> 
                         <Button variant="outline" size="icon">
+                        <div className={`h-3 w-3 rounded-full my-1 ${notifications.find((x: any) => x.read === true) ? ' bg-green-500' : 'bg-neutral-200'}`}></div>
+
                             <Bell className="h-4 w-4" />
                         </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent align="end">
                         {notifications.map((item: any, key: number) => (
-                            <DropdownMenuItem key={key} classNama='p-1 cursor-pointer hover:bg-neutral-50 transition' >
+                            <DropdownMenuItem key={key} className='p-1 cursor-pointer hover:bg-neutral-50 transition flex item-start gap-2'>
+                                <div className={`h-3 w-3 rounded-full my-1 ${!item.read ? ' bg-green-500' : 'bg-neutral-200'}`}></div>
                                 <p>{item.text}</p>
-                                <p>{item.date}</p>
-                                <span> {item.read ? 'Read' : 'Unread'} </span>
+                                <p className="text-xs text-neutral-500">{item.date}</p>
+                                <span>{item.read ? 'Read' : 'Unread'}</span>
                             </DropdownMenuItem>
                         ))}
                     </DropdownMenuContent>
